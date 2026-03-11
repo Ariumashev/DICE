40424374a514b49f552c7d4ab8563a5e11e9e30b
art yum <mailfw464@gmail.com>
2026-03-11 06:50:38 +0300

    FINAL_TEST

--- TEST.cpp
+++ TEST.cpp
@@ -1,27 +1,7 @@
 #include <iostream>
 #include <vector>
 
-int add(int a, int b) {
-    return a + b;
-}
-
-void unusedFunction() {
-    std::vector<int> v = {1, 2, 3};
-    for (int i = 0; i < v.size(); i++) {
-        std::cout << v[i] << std::endl;
-    }
-}
-
 int main() {
-    int x = 0;
-
-    if (x == 0) {
-        std::cout << "hello" << std::endl;
-    }
-
-    int result = add(1, 2);
-
-    std::cout << "result:" << result << std::endl;
-
-    return 0;
+    const int x = 10;
+    std::cout << x << '\n';
 }
